169f790056ede33b1c2c9652326a1c19f97251b6
The Humanoid <>
2021-05-21 23:34:13 +0000

    Added checkUser()

--- index.js
+++ index.js
@@ -7,11 +7,26 @@ exports.checkBot = function(message, command, permissionsArray) {
   command()
   } else {
     try {
-      msg.author.send(`**Missing Permission(s)**\nRequired Permissions:\n\`${permit.join(', ')}\``)
+      msg.author.send(`Required Permissions:\n\`${permit.join(', ')}\``)
     } catch {
       return;
     }
       }
  };
   run(message, command, permissionsArray)
-}
+};
+
+exports.checkUser = function(message, command, permissionsArray) {
+  return message.channel.permissionsFor(message.author.id).has(perms);
+ };
+ function run(msg, cmd, permit) {
+  if (permit.every(permCheck)) {
+  command()
+  } else {
+    try {
+      msg.channel.send(`Required Permissions:\n\`${permit.join(', ')}\``)
+    } catch {
+      return;
+    }
+      }
+};
