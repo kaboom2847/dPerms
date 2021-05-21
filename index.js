45823d83a9102c44f30a64558df12fb0cd1d5c52
The Humanoid <>
2021-05-21 16:32:22 +0000

    Added checkBot()

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+exports.checkBot = function(message, command, permissionsArray) {
+ function permCheck(perms) {
+  return message.channel.permissionsFor(message.guild.me.id).has(perms);
+ };
+ function run(msg, cmd, permit) {
+  if (permit.every(permCheck)) {
+  command()
+  } else {
+    try {
+      msg.author.send(`**Missing Permission(s)**\nRequired Permissions:\n\`${permit.join(', ')}\``)
+    } catch {
+      return;
+    }
+      }
+ };
+  run(message, command, permissionsArray)
 }
